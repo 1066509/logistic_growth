475d5d629562a3fb5be95594dd461ef95091612b
amccarthykerrigan <ava.mccarthykerrigan@stcatz.ox.ac.uk>
2023-12-05 14:12:34 +0000

    Changing annotation wording

--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,7 +1,12 @@
 #Script to plot data and model
 
+#Load packages
+library(ggplot2)
+
+#Read in data set
 growth_data <- read.csv("/cloud/project/experiment1.csv")
 
+#Create logistic growth function for plotting the models' estimated parameters 
 logistic_fun <- function(t) {
   
   N <- (N0*K*exp(r*t))/(K-N0+N0*exp(r*t))
@@ -16,11 +21,15 @@ r <- 0.01004 #This number is from gradient of model1
   
 K <- 60000000000 #This number is from the intercept of model2
 
+
+#Plot the data and model
 ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
-  geom_point()
+  geom_point() +
+  
+  theme_bw()
 
   #scale_y_continuous(trans='log10')
 
